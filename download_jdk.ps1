933fd55956c61bf3d69836e9a972b3b8c58a2819
akashrsgh <akashrsgh@gmail.com>
2025-11-06 22:57:19 +0530

    Create download_jdk.ps1

---
+++ download_jdk.ps1
@@ -0,0 +1,8 @@
+$zipUri = "https://download.oracle.com/java/17/archive/jdk-17.0.12_windows-x64_bin.zip"
+$downloadPath = "C:\\jenkins\\jdk-17.0.12_windows-x64_bin.zip"
+$destinationPath = "C:\\jenkins\\"
+# Download ZIP file
+Invoke-WebRequest -Uri $zipUri -OutFile $downloadPath
+# Unzip the file into the destination folder
+Expand-Archive -Path $downloadPath -DestinationPath $destinationPath
+Rename-Item -Path "C:\\jenkins\\jdk-17.0.12" -NewName "C:\\jenkins\\jdk"
