93736f3788aab92723151ec1d8f391a479e55bd0
akashrsgh <akashrsgh@gmail.com>
2025-11-06 17:57:29 +0530

    Update install_cygwin.ps1

--- install_cygwin.ps1
+++ install_cygwin.ps1
@@ -2,7 +2,7 @@ Invoke-WebRequest -Uri 'https://cygwin.com/setup-x86_64.exe' -OutFile 'C:\\temp\
 $MirrorUrl = "https://cygwin.mirrors.hoobly.com" # Ensure HTTPS is included
 $InstallDir = "C:\cygwin64"
 $DownloadDir = "$env:TEMP\CygwinSetup"
-$Packages = "base,wget,curl" # Always include 'base'
+$Packages = "zip,unzip,wget,curl,make,autoconf,automake,gcc,jdk,libtool,valgrind,ant,subversion,mercurial,git,ruby,nodejs,expect,net-tools,ftp,mc" # Always include 'base'
 
 $Arguments = @(
     "--quiet-mode"
@@ -18,3 +18,11 @@ $Arguments = @(
 
 Start-Process -FilePath 'C:\temp\setup-x86_64.exe' -ArgumentList $Arguments -Wait -NoNewWindow
 Remove-Item -Path 'C:\\temp\\setup-x86_64.exe'
+
+$zipUri = "https://nexus.azulsystems.com/repository/zulu-build-all/jenkins-zulu/zulu/zulu-platform-build/71009/outcome/win64/release/jdk-image.zip"
+$downloadPath = "C:\\jenkins\\jdk-image.zip"
+$destinationPath = "C:\\jenkins\\"
+# Download ZIP file
+Invoke-WebRequest -Uri $zipUri -OutFile $downloadPath
+# Unzip the file into the destination folder
+Expand-Archive -Path $downloadPath -DestinationPath $destinationPath
